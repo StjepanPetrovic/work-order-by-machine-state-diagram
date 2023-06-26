04853785beab2907d938e0966dfefbdb2803b16d
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-26 14:01:49 +0200

    Add project files.

---
+++ STATE_RadniNalog/RadniNalogForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STATE_RadniNalog
+{
+    public partial class RadniNalogForm : Form
+    {
+        private RadniNalog _radniNalog;
+        public RadniNalogForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RadniNalogForm_Load(object sender, EventArgs e)
+        {
+            _radniNalog = new RadniNalog();
+        }
+
+        private void btnZakljucaj_Click(object sender, EventArgs e)
+        {
+            _radniNalog.ZakljucajNalog(txtOpis.Text);
+            txtDatumKreiranja.Text = _radniNalog.DatumKreiranja.ToString();
+        }
+
+        private void btnPredajNalog_Click(object sender, EventArgs e)
+        {
+            _radniNalog.PredajUProizvodnju(dtpDatumPredaje.Value);
+        }
+
+        private void btnZapocniProizvodnju_Click(object sender, EventArgs e)
+        {
+            _radniNalog.ZapocniProizvodnju(dtpDatumPocetka.Value);
+        }
+
+        private void btnDovrsiProizvodnju_Click(object sender, EventArgs e)
+        {
+            _radniNalog.DovrsiProizvodnju(dtpDatumDovrsetka.Value);
+        }
+
+        private void btnOtkaziNalog_Click(object sender, EventArgs e)
+        {
+            _radniNalog.OtkaziNalog();
+        }
+    }
+}
