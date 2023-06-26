6b6ea27e65e0cf2b5b2661ab403d73c69bfe144d
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-26 14:46:26 +0200

    Add enumaration ActivationStates and ActivationEvents

---
+++ STATE_RadniNalog/ProjectStates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STATE_RadniNalog
+{
+	enum ProjectStates
+	{
+		OrderCreated,
+		OrderLocked,
+		PutIntoProduction, 
+		ProductionStarted,
+		ProductionFinished,
+		ProductionCanceled,
+	}
+}
