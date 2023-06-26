f472b386c006bd7df8c9457ed1ce9db2663836c6
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-26 15:23:35 +0200

    Add instance of StateManager in RadniNalog class and call making transitions on actions

--- STATE_RadniNalog/RadniNalog.cs
+++ STATE_RadniNalog/RadniNalog.cs
@@ -13,31 +13,36 @@ namespace STATE_RadniNalog
         public DateTime DatumPredaje { get; set; }
         public DateTime DatumPocetka { get; set; }
         public DateTime DatumDovrsetka { get; set; }
+        public StateManager StateManager { get; set; } = new StateManager();
 
         public void ZakljucajNalog(string opis)
         {
             Opis = opis;
             DatumKreiranja = DateTime.Now;
+            StateManager.MakeTransition(ActivationEvents.AllItemsAddedInOrder);
         }
 
         public void PredajUProizvodnju(DateTime datumPredaje)
         {
             DatumPredaje = DateTime.Now;
+            StateManager.MakeTransition(ActivationEvents.ProductionCommited);
         }
 
         public void ZapocniProizvodnju(DateTime datumPocetka)
         {
             DatumPocetka = datumPocetka;
+            StateManager.MakeTransition(ActivationEvents.OrderTaken);
         }
 
         internal void DovrsiProizvodnju(DateTime datumDovrsetka)
         {
             DatumDovrsetka = datumDovrsetka;
+            StateManager.MakeTransition(ActivationEvents.AllItemsFinished);
         }
 
         public void OtkaziNalog()
         {
-
+            StateManager.MakeTransition(ActivationEvents.GivingUpProduction);
         }
     }
 }
